bfb363f7560c7b294ec16ba7f80945ff2e34987b
Timofey <timofeykropotov@gmail.com>
2024-06-07 14:12:22 +0300

    add author and ad serializers

---
+++ parser/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from api.models import Author, Advt
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+
+class AdvtSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer(read_only=True)
+
+    class Meta:
+        model = Advt
+        fields = '__all__'
