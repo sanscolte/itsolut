36cba93af9744b79a1b5abb771d7517ac9a638c2
Timofey <timofeykropotov@gmail.com>
2024-06-07 14:10:09 +0300

    register models in admin.py

--- parser/api/admin.py
+++ parser/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Author, Advt
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name'
+    list_display_links = 'name',
+
+
+@admin.register(Advt)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'author', 'views', 'position'
+    list_display_links = 'title',
+    ordering = 'position',
