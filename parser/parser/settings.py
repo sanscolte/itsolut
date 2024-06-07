d615411b6805f73c7d1e60845533ce8404b3f262
Timofey <timofeykropotov@gmail.com>
2024-06-07 07:39:54 +0300

    change db to postgres

--- parser/parser/settings.py
+++ parser/parser/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'parser.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "itsolut",
+        "USER": "postgres",
+        "PASSWORD": "lovelove",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
